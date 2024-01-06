c05881b3d2f2f5eef9aa14efee0ed89c05a06d2e
Max <max.funoff@gmail.com>
2024-01-06 14:40:18 +0200

    chore: build gutenberg components

--- assets/js/dist/index.asset.php
+++ assets/js/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-edit-post', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'a179353f74c6636749ee');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-edit-post', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '1aceffa4c691bd46d41d');
